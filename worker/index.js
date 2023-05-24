67efb779902d1c370499fe4c3eafa19f327aa48a
Thomas Verschoren <thomas@verschoren.com>
2023-05-24 16:05:39 +0200

    deploy worker

---
+++ worker/index.js
@@ -0,0 +1,59 @@
+export default {
+  async fetch(request, env, ctx) {
+    const { url } = request;
+    if (request.method === "POST") {
+      if (url.includes("notification")) {
+
+        const input = await request.json();
+
+        if (input.phone){
+          var payload = {
+            "destination": {
+                "integrationId": env.integration_id,
+                "destinationId": input.phone
+            },
+            "author": {
+                "role": "appMaker"
+            },
+            "messageSchema": "whatsapp",
+            "message": {
+                "type": "template",
+                "template": {
+                    "namespace": "XXXXXXXX_XXXX_XXXX_XXXX_XXXXXXXXXXXX",
+                    "name": "subscribing",
+                    "language": {
+                        "policy": "deterministic",
+                        "code": "en"
+                    }
+                }
+            }
+          }
+          console.log(payload);
+
+          const api_endpoint = `https://api.smooch.io/v1.1/apps/${env.app_id}/notifications`
+          const key = btoa(env.key_id + ":" + env.secret_key);
+
+          const init = {
+            body: JSON.stringify(payload),
+            method: "POST",
+            headers: {
+              "content-type": "application/json;charset=UTF-8",
+              "authorization": "Basic " + key
+            },
+          };
+
+          const response = await fetch(api_endpoint, init);
+          const results = await response.json();
+          console.log(results);
+          return new Response(JSON.stringify(result), init);
+        } else {
+          return new Response("No Phone provided", {status:400});
+        }
+      } else {
+        return new Response ("No accessible path", {status:400});
+      }
+    } else if (request.method === "GET") {
+      return new Response("The request was a GET");
+    }
+  },
+};
